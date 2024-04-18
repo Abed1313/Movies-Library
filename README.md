1e8fe28023577ba149a98ef46c56b6418e8be490
Abed1313 <abedradwan84.5@gmail.com>
2024-04-18 01:02:01 +0000

    lab12

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 **Author Name**: Abdel Rahman Radwan
 
 ## WRRC
+LAB11:
 ![WRRC Image](lab11.PNG)
+LAB12:
+![WRRC Imge](lap12.PNG)
 
 ## Overview
 this project is a movie library app.
@@ -29,13 +32,33 @@ http://localhost:3000/
 List Movie Recipes:
 1.GET '/' for home page 
 2.GET /favorite
+3.Trending Page Endpoint: /trending
+Fetches and displays trending movies of the week.
 
-Error Handling:
+4.Search Page Endpoint: /search
+Allows users to search for movies based on the provided query.
+
+5.Popular Page Endpoint: /Popular
+Fetches and displays Popular movies.
+
+6.Countries Page Endpoint: /Countries
+Fetches and displays Countries movies.
+
+## Error Handling:
 1.404 Not Found: Automatically handles any undefined routes with a JSON response indicating the error.
 2.500 Internal Server Error: Captures and logs server errors, providing a JSON response for unexpected conditions.
 
-Technical Specifications:
+## Technical Specifications:
 1.Framework: Utilizes Express.js, a minimal and flexible Node.js web application framework.
 2.Data Handling: Reads movie data from a local JSON file (movieData/data.json), simulating a database for simplicity.
 3.Data Model: Uses a simple JavaScript constructor function (Recipe) to model each movie data entry.
 4.Server: Configured to run on port 3000, making the application accessible locally for development and testing.
+
+## Dependencies
+Express
+Cors
+Axios
+Dotenv
+
+## API Key
+Make sure to set up your API key in a .env file and name the variable API_KEY.
--- server.js
+++ server.js
@@ -2,27 +2,97 @@
 const express = require("express");
 //2.inv;e express
 const app =express();
-const port = 3000;
+
+const axios= require("axios");
+require("dotenv").config();
+const port = process.env.PORT;
+const ABI_KEY = process.env.ABI_KEY;
 
 const recipesData=require('./movieData/data.json');
 
 
                  /// from json file ///
 app.get('/', recipesHandeler);
+app.get("/trending",trendingHandelar);
+app.get("/search", searchHandelar);
+app.get("/Popular",popularHandelar);
+app.get("/Countries",countriesHandeler);
+
 function recipesHandeler(req,res){
     const result=[];
     recipesData.data.forEach(e => {
-        let singelRecipes =new Recipe(e.title,e.poster_path,e.overview);
+        let singelRecipes =new MovieLab11(e.title,e.poster_path,e.overview);
         result.push(singelRecipes);
     });
 res.json(result)
 }
 
-function Recipe(title,poster_path,overview){
-this.title=title;
-this.poster_path=poster_path;
-this.overview=overview;
+function trendingHandelar(req,res){
+let url =`https://api.themoviedb.org/3/trending/all/week?api_key=${ABI_KEY}&language=en-US`
+axios.get(url)
+.then(result=>{
+    let response = result.data.results.map(x =>{
+       return new MovieLab12(x.id,x.title,x.release_date,x.poster_path,x.overview) 
+    })
+    res.json(response)
+})
+.catch(error=>{
+    console.log(error);
+})
+
 }
+function searchHandelar(req,res){
+let title=req.query.title;
+let url =`https://api.themoviedb.org/3/search/movie?query=${title}&api_key=${ABI_KEY}&language=en-US`
+axios.get(url)
+.then(result=>{
+    let responsee=result.data.results
+    res.json(responsee);
+})
+.catch(error=>{
+    console.log(error);
+})
+}
+function popularHandelar(req,res){
+    let url =`https://api.themoviedb.org/3/person/popular?&api_key=${ABI_KEY}&language=en-US`
+axios.get(url)
+.then(result=>{
+    console.log(result.data.results);
+    let response = result.data.results
+    res.json(response)
+})
+.catch(error=>{
+    console.log(error);
+    res.status(500).send("internal server error2");
+})
+}
+function countriesHandeler(req,res){
+    let url =`https://api.themoviedb.org/3/configuration/countries?&api_key=${ABI_KEY}&language=en-US`
+axios.get(url)
+.then(result=>{
+    console.log(result.data.results);
+    let response = result.data.results
+    res.json(response)
+})
+.catch(error=>{
+    console.log(error);
+    res.status(500).send("internal server error");
+})
+}
+
+function MovieLab11(title,poster_path,overview){
+    this.title=title;
+    this.poster_path=poster_path;
+    this.overview=overview;
+    }
+
+    function MovieLab12(id,title,release_date,poster_path,overview){
+        this.id=id;
+        this.title=title;
+        this.release_date=release_date;
+        this.poster_path=poster_path;
+        this.overview=overview;
+    }
 
 app.get("/favorite",(req,res) =>{
     res.send("Welcome to Favorite Page")
